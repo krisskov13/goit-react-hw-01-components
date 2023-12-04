de9f7423681fe99b04360978b93fd3bb1e9cf10b
krisskov13 <kristinakovalenko333@gmail.com>
2023-12-04 21:11:42 +0200

    add Profile

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Profile } from './components/Profile/Profile';
+
+export const App = () => {
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,9 @@
+import style from './Profile.module.css';
+
+export const Profile = () => {
+  return (
+    <div>
+      <p>Hello</p>
+    </div>
+  );
+};
