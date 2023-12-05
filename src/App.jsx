63d29e38018b40675479aec95c84160d626e4af0
krisskov13 <kristinakovalenko333@gmail.com>
2023-12-05 22:00:12 +0200

    fixed errors

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { Statistics } from 'components/Statistics/Statistics';
 import { Profile } from './components/Profile/Profile';
 import { FriendList } from 'components/FriendList/FriendList';
 import { TransactionHistory } from 'components/Transaction/Transaction';
-import user from './components/Profile/user.json';
-import data from './components/Statistics/data.json';
-import friends from './components/FriendList/friends.json';
-import transactions from './components/Transaction/transactions.json';
+import user from './data/user.json';
+import data from './data/data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
 export const App = () => {
   return (
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,7 +3,7 @@ import css from './Statistics.module.css';
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
 
       <ul className={css.statistics_list}>
         {stats.map(statistics => (
